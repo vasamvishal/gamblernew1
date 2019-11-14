3ccfd725eca459563be22f098b95a8e41454399f
Admin <you@example.com>
2019-11-14 16:04:05 +0530

    Added usercase6  to calculate no of luckydays and no of unlucky day

--- gamble.sh
+++ gamble.sh
@@ -1,17 +1,23 @@
 #welcome gambler
 STAKES=100
-DAYS=20
+DAYS=4
 bet=1
 stake=100
 WIN=1
+luckyday=0
+unluckday=0
 LOSS=0
+total=0;
+losstotal=0
 totalamount=0
+declare -A dictionary
 function percentage()
 {
 maxlimit=$(( $cash+$cash/2 ))
 minlimit=$(( $cash/2 ))
 
 }
+
 function gamble()
 {
 cash=$STAKES
@@ -41,17 +47,41 @@ do
    done
    if [ $STAKES == $minlimit ]
       then
+
           totalamount=$(( $totalamount-$minlimit ))
+          dictionary["day"$count]=$totalamount
       else
           diff=$(( $maxlimit-$stake ))
           totalamount=$(( $totalamount+$diff ))
+	 dictionary["day"$count]=$totalamount
    fi
 bet=1;
 STAKES=100;
+
 done
+
+ luckyDay=$(printf "%s\n" ${dictionary[@]} | sort -nr | head -1 )
+ unLuckyDay=$(printf "%s\n" ${dictionary[@]} | sort -nr | tail -1 )
+ 
+ for i in ${!dictionary[@]}
+ do
+   echo $i ":" ${dictionary[$i]}
+ done
+ for k in ${!dictionary[@]}
+ do 
+   if [  ${dictionary[$k]} -eq $luckyDay ]
+   then
+	echo LuckyDay : $k Amount : $luckyDay
+   fi
+   if [ ${dictionary[$k]} -eq $unLuckyDay ] 
+   then
+      echo UnLuckyDay : $k Amount : $unLuckyDay
+   fi
+ done
+
 echo "no of losses" $loss
 echo "no of wins" $win
-echo "totalamount" $totalamount
+
 
 }
 gamble
