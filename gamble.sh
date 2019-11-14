b2cc7c9b786648d37349b6d2e46ecb92e208e35c
Admin <you@example.com>
2019-11-14 11:15:56 +0530

    Added usercase2 to check for win or loss

--- gamble.sh
+++ gamble.sh
@@ -1,3 +1,13 @@
 #welcome gambler
 STAKES=100
-BET=1
+bet=1
+WIN=1
+LOSS=0
+random=$(( RANDOM%2 ))
+bet=$(( $bet +1 ))
+if [ $random -eq $WIN ]
+   then 
+        echo "Win"
+   else
+        echo "Loss"
+fi
