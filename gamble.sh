1ef5911b362c572ca44dfed7e5e385740f4b580b
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-15 18:55:02 +0530

    Added final_changes sorted identation and grouped  variables as constant and variables

--- gamble.sh
+++ gamble.sh
@@ -1,90 +1,92 @@
 #welcome gambler
+#CONSTANT
 STAKES=100
 DAYS=6
-bet=1
-stake=100
+BETAMOUNT=1
 WIN=1
+LOSS=0
+
+#variable
+stake=$STAKES
 luckyday=0
 unluckday=0
-LOSS=0
 total=0;
+bet=$BETAMOUNT
 losstotal=0
 totalamount=0
 
-declare -A dictionary
+declare -A daysValue
 function percentage()
 {
-maxlimit=$(( $cash+$cash/2 ))
-minlimit=$(( $cash/2 ))
-
+	maxlimit=$(( $cash+$cash/2 ))
+	minlimit=$(( $cash/2 ))
 }
 
-function gamble()
+function gambling()
 {
-cash=$STAKES
-percentage
-for (( count=0;count<DAYS;count++ ))
-do
+	cash=$STAKES
+	percentage
+	for (( count=0;count<$DAYS;count++ ))
+	do
    while [ $bet!=0 ]
    do
-    bet=$(( $bet +1 ))
-    random=$(( RANDOM%2 ))
-     if [ $random -eq $WIN ]
-        then 
-            STAKES=$(( $STAKES+1 ))
-	if [ $STAKES -ge $maxlimit ]
-           then
-	       win=$(( $win+1 ))
-	       break;
+   bet=$(( $bet +1 ))
+   random=$(( RANDOM%2 ))
+   if [ $random -eq $WIN ]
+   then 
+		cash=$(( $cash+1 ))
+	if [ $cash -ge $maxlimit ]
+   then
+		win=$(( $win+1 ))
+	   break;
 	fi
-     else
-        STAKES=$(( $STAKES-1 ))
-        if [ $STAKES -le $minlimit ]
-	   then
-              loss=$(( $loss+1 ))
-	      break
+   else
+      cash=$(( $cash-1 ))
+   if [ $cash -le $minlimit ]
+	then
+      loss=$(( $loss+1 ))
+	   break
 	fi
-     fi
-   done
-   if [ $STAKES == $minlimit ]
-      then
-
-          totalamount=$(( $totalamount-$minlimit ))
-          dictionary["day"$count]=$totalamount
-      else
-          diff=$(( $maxlimit-$stake ))
-          totalamount=$(( $totalamount+$diff ))
-	 dictionary["day"$count]=$totalamount
    fi
-bet=1;
-STAKES=100;
-done
-function luckday(){
- luckyDay=$(printf "%s\n" ${dictionary[@]} | sort -nr | head -1 )
- unLuckyDay=$(printf "%s\n" ${dictionary[@]} | sort -nr | tail -1 ) 
- echo $totalamount 
- for i in ${!dictionary[@]}
-   do
-     echo $i ":" ${dictionary[$i]}
    done
- for k in ${!dictionary[@]}
+   if [ $cash == $minlimit ]
+   then
+		totalamount=$(( $totalamount-$minlimit ))
+      dictionary["day"$count]=$totalamount
+   else
+      diff=$(( $maxlimit-$stake ))
+      totalamount=$(( $totalamount+$diff ))
+	   dictionary["day"$count]=$totalamount
+   fi
+      bet=1;
+      cash=100;
+done
+function searchingDictionary(){
+	profitValue=$(printf "%s\n" ${daysValue[@]} | sort -nr | head -1 )
+	lossValue=$(printf "%s\n" ${daysValue[@]} | sort -nr | tail -1 ) 
+	echo $totalamount 
+	for i in ${!daysValue[@]}
+	do
+ 		echo $i ":" ${daysValue[$i]}
+   done
+	for k in ${!daysValue[@]}
    do 
-     if [  ${dictionary[$k]} -eq $luckyDay ]
-        then
-	   echo LuckyDay : $k Amount : $luckyDay
-     fi
-     if [ ${dictionary[$k]} -eq $unLuckyDay ] 
-        then
-            echo UnLuckyDay : $k Amount : $unLuckyDay
-     fi
- done
+	if [  ${daysValue[$k]} -eq $profitValue ]
+	then
+	   echo LuckyDay : $k Amount : $profitValue
+   fi
+	if [ ${daysValue[$k]} -eq $lossValue ] 
+   then
+		echo UnLuckyDay : $k Amount : $lossValue
+   fi
+ 	done
 }
 echo "no of losses" $loss
 echo "no of wins" $win
 }
-  while [ $totalamount -ge -1 ]
+	while [ $totalamount -ge -1 ]
    do 
-   gamble
-   luckday
-    done
+   gambling
+   searchingDictionary
+   done
 
