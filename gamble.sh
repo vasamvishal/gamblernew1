4e0e686b6c0bacfacead881762f9d3004c65de83
SarswatiPatil <dipakalagate1991@gmail.com>
2019-11-16 11:37:30 +0530

    Added idendation proper as per requirement

--- gamble.sh
+++ gamble.sh
@@ -28,65 +28,66 @@ function gambling()
 	percentage
 	for (( count=0;count<$DAYS;count++ ))
 	do
-   while [ $bet!=0 ]
-   do
-   bet=$(( $bet +1 ))
-   random=$(( RANDOM%2 ))
-   if [ $random -eq $WIN ]
-   then 
-		cash=$(( $cash+1 ))
-	if [ $cash -ge $maxlimit ]
-   then
-		win=$(( $win+1 ))
-	   break;
-	fi
-   else
-      cash=$(( $cash-1 ))
-   if [ $cash -le $minlimit ]
-	then
-      loss=$(( $loss+1 ))
-	   break
-	fi
-   fi
-   done
-   if [ $cash == $minlimit ]
-   then
-		totalamount=$(( $totalamount-$minlimit ))
-      dictionary["day"$count]=$totalamount
-   else
-      diff=$(( $maxlimit-$stake ))
-      totalamount=$(( $totalamount+$diff ))
-	   dictionary["day"$count]=$totalamount
-   fi
-      bet=1;
-      cash=100;
-done
-function searchingDictionary(){
+   		while [ $bet!=0 ]
+   		do
+   			bet=$(( $bet +1 ))
+   			random=$(( RANDOM%2 ))
+   			if [ $random -eq $WIN ]
+   			then 
+				cash=$(( $cash+1 ))
+			if [ $cash -ge $maxlimit ]
+   			then
+				win=$(( $win+1 ))
+	   		break;
+			fi
+   			else
+      				cash=$(( $cash-1 ))
+   			if [ $cash -le $minlimit ]
+			then
+      				loss=$(( $loss+1 ))
+	   		break
+			fi
+   			fi
+   		done
+   			if [ $cash == $minlimit ]
+   			then
+				totalamount=$(( $totalamount-$minlimit ))
+      				daysValue["day"$count]=$totalamount
+   			else
+      				diff=$(( $maxlimit-$stake ))
+      				totalamount=$(( $totalamount+$diff ))
+	   			daysValue["day"$count]=$totalamount
+   			fi
+      				bet=1;
+      				cash=100;
+	done
+}
+
+function searchingDictionary()
+{
 	profitValue=$(printf "%s\n" ${daysValue[@]} | sort -nr | head -1 )
 	lossValue=$(printf "%s\n" ${daysValue[@]} | sort -nr | tail -1 ) 
 	echo $totalamount 
 	for i in ${!daysValue[@]}
 	do
  		echo $i ":" ${daysValue[$i]}
-   done
+   	done
 	for k in ${!daysValue[@]}
-   do 
-	if [  ${daysValue[$k]} -eq $profitValue ]
-	then
-	   echo LuckyDay : $k Amount : $profitValue
-   fi
-	if [ ${daysValue[$k]} -eq $lossValue ] 
-   then
-		echo UnLuckyDay : $k Amount : $lossValue
-   fi
+  	do 
+		if [  ${daysValue[$k]} -eq $profitValue ]
+		then
+	   		echo LuckyDay : $k Amount : $profitValue
+   		fi
+		if [ ${daysValue[$k]} -eq $lossValue ] 
+   		then
+			echo UnLuckyDay : $k Amount : $lossValue
+  		fi
  	done
 }
-echo "no of losses" $loss
-echo "no of wins" $win
-}
+
 	while [ $totalamount -ge -1 ]
-   do 
-   gambling
-   searchingDictionary
-   done
+   	do 
+   		gambling
+   		searchingDictionary
+   	done
 
